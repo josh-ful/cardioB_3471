8d52956eaf0ef45f6a5fee5159986dc7e6ffbf36
josh_fu <jwfulton221@gmail.com>
2025-03-18 15:18:23 -0500

    overhaul of formatting and design to log in screen

--- demodemoDemo/src/main/java/SoftwareEngineering/MainMenu.java
+++ demodemoDemo/src/main/java/SoftwareEngineering/MainMenu.java
@@ -24,31 +24,111 @@ import java.awt.*;
 
 public class MainMenu {
 
-
+    public class companyDetail extends JPanel {
+        public companyDetail() {
+            JLabel companyName = new JLabel("CardioB™");
+            companyName.setFont(new Font("Comic Sans MS", Font.BOLD, 70));
+            companyName.setForeground(Color.BLACK);
+            //companyName.setHorizontalAlignment(SwingConstants.CENTER);
+            this.add(companyName);
+        }
+    }
     public class buttons extends JPanel {
         public buttons() {
             this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
             JButton loginButton = new JButton("Login");
             loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            loginButton.setMaximumSize(new Dimension(200, 50));
+            loginButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+
             JButton registerButton = new JButton("Register");
             registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            this.add(Box.createVerticalStrut(200));
+            registerButton.setMaximumSize(new Dimension(200, 50));
+            registerButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+
             this.add(loginButton);
+            this.add(Box.createVerticalStrut(25));
             this.add(registerButton);
         }
     }
 
     public void createAndShowGUI() {
         JFrame frame = new JFrame();
+        //frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("CardioB ™");
+        frame.setTitle("Trello Fellows®");
+
+        frame.setSize(450, 800);
+
+        JLabel companyName = new JLabel("CardioB");
+        companyName.setFont(new Font("Comic Sans MS", Font.BOLD, 70));
+        companyName.setForeground(Color.BLACK);
+        companyName.setAlignmentY(Component.CENTER_ALIGNMENT);
+        companyName.setHorizontalAlignment(SwingConstants.CENTER);
+        companyName.setVerticalAlignment(SwingConstants.TOP);
+
+        JButton loginButton = new JButton("Login");
+        //loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.setMaximumSize(new Dimension(100, 50));
+        loginButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+        //loginButton.setHorizontalAlignment(SwingConstants.TOP);
+
+        JButton registerButton = new JButton("Register");
+        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerButton.setMaximumSize(new Dimension(100, 50));
+        registerButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+        //registerButton.setSize(20,20);
+
+        ImageIcon icon = new ImageIcon("src/main/resources/cardioB_logo.png");
+        Image image = icon.getImage();
+        image = image.getScaledInstance(600, 200, Image.SCALE_SMOOTH);
+        icon = new ImageIcon(image);
+        JLabel iconLabel = new JLabel(icon);
 
 
-        frame.setSize(800, 600);
-        frame.add(new buttons());
+        c.anchor = GridBagConstraints.PAGE_END;
+
+
+
+
+        //frame.add(iconLabel, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipady = 100;
+        frame.add(iconLabel, c);
+
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.ipady = 10;
+        frame.add(companyName, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 5;
+        frame.add(loginButton, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 15;
+        frame.add(registerButton, c);
+
+
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
+
+
     }
 
     public static void main(String[] args) {
