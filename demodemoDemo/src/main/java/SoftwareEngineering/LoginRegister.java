fc96903d75a08d4e2c15e03433dbfcd11cec9512
Dysous <68669417+Dysous@users.noreply.github.com>
2025-03-20 13:54:56 -0500

    Commit

--- demodemoDemo/src/main/java/SoftwareEngineering/LoginRegister.java
+++ demodemoDemo/src/main/java/SoftwareEngineering/LoginRegister.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LoginRegister {
+    
     public void createAndShowGUI(JFrame frame) {
         Map<String,String> logins  = new LinkedHashMap<>();
         logins.put("Noah", "Mathew");
@@ -16,8 +17,8 @@ public class LoginRegister {
         logins.put("Emily", "Wokoek");
         logins.put("Carter", "Lewis");
         logins.put("Lawson", "Hale");
-
-        //JFrame frame = new JFrame();
+        
+        //JFrame frame = frames;
         //frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -35,10 +36,69 @@ public class LoginRegister {
         companyName.setVerticalAlignment(SwingConstants.TOP);
 
         JButton loginButton = new JButton("Login");
+        loginButton.setMaximumSize(new Dimension(100, 50));
+        loginButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+        loginScene(frame, loginButton, logins);
+
+        JButton registerButton = new JButton("Register");
+        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerButton.setMaximumSize(new Dimension(100, 50));
+        registerButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+        registerScene(registerButton, logins);
+        
+        
+        ImageIcon icon = new ImageIcon("src/main/resources/cardioB_logo.png");
+        Image image = icon.getImage();
+        image = image.getScaledInstance(600, 200, Image.SCALE_SMOOTH);
+        icon = new ImageIcon(image);
+        JLabel iconLabel = new JLabel(icon);
+
+
+        c.anchor = GridBagConstraints.PAGE_END;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipady = 100;
+        frame.add(iconLabel, c);
+
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.ipady = 10;
+        frame.add(companyName, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 5;
+        frame.add(loginButton, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 15;
+        frame.add(registerButton, c);
+
+
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+
+    }
+
+    private static void loginScene(JFrame frame, JButton loginButton, Map<String, String> logins) {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                //JFrame frame = new JFrame("Login");
+
+                frame.removeAll();
+                frame.revalidate();
+                frame.repaint();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setTitle("Login ™");
                 JPanel panel = new JPanel();
@@ -80,18 +140,11 @@ public class LoginRegister {
                 panel.add(loginButton);
                 frame.add(panel);
                 frame.setVisible(true);
-
-                //repaint();
-
             }
         });
+    }
 
-        //loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        loginButton.setMaximumSize(new Dimension(100, 50));
-        loginButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
-        //loginButton.setHorizontalAlignment(SwingConstants.TOP);
-
-        JButton registerButton = new JButton("Register");
+    private static void registerScene(JButton registerButton, Map<String, String> logins) {
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -139,59 +192,6 @@ public class LoginRegister {
                 frame.setVisible(true);
             }
         });
-
-        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        registerButton.setMaximumSize(new Dimension(100, 50));
-        registerButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
-        //registerButton.setSize(20,20);
-
-        ImageIcon icon = new ImageIcon("src/main/resources/cardioB_logo.png");
-        Image image = icon.getImage();
-        image = image.getScaledInstance(600, 200, Image.SCALE_SMOOTH);
-        icon = new ImageIcon(image);
-        JLabel iconLabel = new JLabel(icon);
-
-
-        c.anchor = GridBagConstraints.PAGE_END;
-
-
-
-
-        //frame.add(iconLabel, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.ipady = 100;
-        frame.add(iconLabel, c);
-
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 3;
-        c.ipady = 10;
-        frame.add(companyName, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 5;
-        frame.add(loginButton, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 15;
-        frame.add(registerButton, c);
-
-
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
-
-
     }
 
 }
