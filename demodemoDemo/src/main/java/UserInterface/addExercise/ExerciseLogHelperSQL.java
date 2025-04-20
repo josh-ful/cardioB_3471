6111081d93aa343f7869d8a3aa911177969bb896
Lawson <70649217+halelj49@users.noreply.github.com>
2025-04-19 19:53:30 -0500

    Divided reader into SQL reader and CSV reader. Also made UserController actually do things instead of pretending to. I wanted some polymorphism with the ExerciseLogHelper but I couldn't figure it out. It really wants to be static, so polymorphism is generally out of the question. Also there's a backup for the csv file, but I'm unsure if people actually use that lol

---
+++ demodemoDemo/src/main/java/UserInterface/addExercise/ExerciseLogHelperSQL.java
@@ -0,0 +1,11 @@
+package UserInterface.addExercise;
+
+
+// TODO: implement backend stuff for exerciseLogHelperSQL
+public class ExerciseLogHelperSQL extends ExerciseLogHelper {
+    public ExerciseLogHelperSQL() {
+        super();
+    }
+
+
+}
