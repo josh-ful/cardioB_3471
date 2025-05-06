1b44d4efdf9dd5d6bd6976a63512117ac6c40809
Lawson <70649217+halelj49@users.noreply.github.com>
2025-05-06 11:56:31 -0500

    Updated MetricTypes

--- demodemoDemo/src/main/java/UserInformation/DailyMetrics/MetricTypes.java
+++ demodemoDemo/src/main/java/UserInformation/DailyMetrics/MetricTypes.java
@@ -1,5 +1,18 @@
 package UserInformation.DailyMetrics;
 
 public enum MetricTypes {
-    WEIGHT, SLEEP, CALORIES, WKTDURATION;
+    WEIGHT("weight"),
+    SLEEP("sleep"),
+    CALORIES("calories"),
+    WKTDURATION("wktduration"),;
+
+    public String getName() {
+        return name;
+    }
+
+    private final String name;
+
+    MetricTypes(String name) {
+        this.name = name;
+    }
 }
